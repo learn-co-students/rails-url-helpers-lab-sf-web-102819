d5aa2b3412ba69addf98b24c6a6e36e94641e988
Allen Shin <geeuho@gmail.com>
2019-11-23 15:35:04 -0800

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,7 +4,7 @@ describe Student do
   before(:each) do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
-
+ 
   it 'can be created' do
     expect(@student).to be_valid
   end
